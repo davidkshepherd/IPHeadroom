cf589c280884fb81c11cdb87025d655170526f64
David Shepherd <shepherd@Davids-MacBook-Pro-3.local>
2021-01-29 22:54:38 -0500

    master

--- ipheadroomplot.py
+++ ipheadroomplot.py
@@ -5,14 +5,9 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Create 2 new lists height and weight
-height = [1.87,  1.87, 1.82, 1.91, 1.90, 1.85]
-weight = [81.65, 97.52, 95.25, 92.98, 86.18, 88.45]
-
-# Import the numpy package as np
-import numpy as np
-
+'''
 ## Functions
+'''
 def getFreeIPPercent(x,y):
     return "{:.2%}".format(x / y)
 
@@ -29,15 +24,11 @@ def getFreeIPCount(x):
     return subnets.AvailableIpAddressCount[x]
 
 def getUsedIPPercent(x,y):
-#    z = x / y
-    '''
-    return  "{:.2%}".format(x / y)
-    '''
-    return  x / y
-
+    return x / y
 ## End Functions
-
-
+'''
+Set up Lists to hold created data
+'''
 cidrList = []
 SubnetSizeList = []
 AWSSubnetSizeList = []
@@ -47,29 +38,35 @@ FreeIPPercentList = []
 UsedIPPercentList = []
 
 '''
-cidrList.append("10.0.0.0/23")
-AWSSubnetSizeList.append(507)
-'''
-
-
-'''
-print(cidrList)
-print(AWSSubnetSizeList)
-print(cidrList[0])
-print(AWSSubnetSizeList[0])
-'''
-
-## Dataload
-## Collect the name of te subnets file.
+## Dataload and choices
+## Collect the name of the subnets file.
 ## If no input use the default file name "subnets.csv"
+## Collect user choices on displays
+## Choices default to Yes
+'''
 defaultsubnetsfile = "subnets.csv"
-subnetfile = input("Input your subnet file name (Enter for the default " + defaultsubnetsfile + "):")
+
+print("=" * 41, " IP Headroom Tool ", "=" * 41)
+print("=" *2)
+subnetfile = input("== Input your subnet file name (Enter for the default " + defaultsubnetsfile + "): ")
+print("=" *2)
+showtable = input("== Do you want to see a Results Table?  Y/n: ")
+print("=" *2)
+sample = input("== Sample 20 subnets?  Y/n: ")
+print("=" *2)
+showbar = input("== Do you want to see a Bar Chart?  Y/n: ")
+print("=" *2)
+showscatter = input("== Do you want to see a Scatter Plot?  Y/n: ")
+print("=" * 41, " IP Headroom Tool ", "=" * 41)
+print("")
+print("=" * 46, " Results ", "=" * 46)
+
+
 
 if not subnetfile:
     subnets = pd.read_csv(defaultsubnetsfile)
 else:
-#    if os.path.exists(subnetfile):
-        subnets = pd.read_csv(subnetfile)
+    subnets = pd.read_csv(subnetfile)
 
 i = 0
 #while i < 3:
@@ -115,53 +112,44 @@ for x in subnets.CidrBlock:
 
 
 ## loop through the csv get get valuse, create more values and add to your lists
-    print(UsedIPCountList[i])
-
+##    print(UsedIPCountList[i])
+#    showtable = input("Do you want to see a Results Table?  Y/N: ")
+    if showtable not in ('n', 'N'):
+        if i == 0:
+            print("###", "Subnet ID".ljust(30, " "), "CIDR".ljust(20, " "), "CIDR Size".rjust(10, " "), "Used IP #".rjust(10, " "), "Used IP %".rjust(8, " "), sep=" | ")
+            print(str(i +1 ).ljust(3, " "),subnets.SubnetId[i].ljust(30, " "), cidr.ljust(20, " "), str(f'{AWSSubnetSize:,}').rjust(10, " "), str(f'{UsedIPCount:,}').rjust(10, " "), str("{00:.2%}".format(UsedIPPercent)), sep=" | " )
+        else:
+            print(str(i + 1).ljust(3, " "),subnets.SubnetId[i].ljust(30, " "), cidr.ljust(20, " "), str(f'{AWSSubnetSize:,}').rjust(10, " "), str(f'{UsedIPCount:,}').rjust(10, " "), str("{00:.2%}".format(UsedIPPercent)), sep=" | " )
     i += 1
-    if i >= 10:
-        break
-'''
-print(UsedIPCountList[i])
-'''
 
-'''
-N = 1
-x = np.random.rand(N)
-y = np.random.rand(N)
-'''
-'''
-x = AWSSubnetSizeList
-y = SubnetSizeList
-'''
-x = UsedIPCountList
-y = FreeIPCountList
-
-colors = np.random.rand(i)
-plt.scatter(x, y, c=colors, alpha=0.5)
-plt.xlabel("AWS Subnet Size")
-plt.ylabel("Subnet Size")
-plt.show()
-
-
-x = FreeIPCountList
-y = cidrList
-plt.bar(x,y)
-plt.xlabel("Free IPs")
-plt.ylabel("CIDR")
-
-plt.show()
+    if sample not in ('n', 'N'):
+        if i >= 20:
+            break
 
 
 
-'''
-# Create 2 numpy arrays from height and weight
-np_height = np.array(height)
-np_weight = np.array(weight)
+## Set up Bar Chart if requested
+if showbar not in ('n', 'N'):
+    y = FreeIPCountList
+    x = cidrList
+#    plt.figure(figsize=(10,8))
+    fig= plt.figure(figsize=(9,7))
+    plt.barh(x,y)
+    plt.xlabel("Free IPs")
+    plt.ylabel("CIDR")
+    plt.show()
+else:
+    print("OK.  Maybe next time?")
 
-print(np_height[0])
-
-
-thislist = ["apple", "banana", "cherry"]
-thislist.append("orange")
-print(thislist)
-'''
+## Set up Scatter Chart
+if showscatter not in ('n', 'N'):
+    x = UsedIPCountList
+    y = FreeIPCountList
+    colors = np.random.rand(i)
+    fig= plt.figure(figsize=(9,7))
+    plt.scatter(x, y, c=colors, alpha=0.5)
+    plt.xlabel("Used IP Count")
+    plt.ylabel("Free IP Count")
+    plt.show()
+else:
+    print("OK.  Maybe next time?")
